4d3280b4c14cb9259334dc49d8af213694a40a07
Roel Stalman <roel@badgeville.com>
2018-11-23 14:27:37 -0800

    Initial commit

---
+++ src/main/java/Money.java
@@ -0,0 +1,44 @@
+class Money implements Expression {
+    protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    static Money dollar(int amount){
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount){
+        return new Money(amount, "CHF");
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+            && currency().equals(money.currency());
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    Expression plus(Money addend) {
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(String to) {
+        return this;
+    }
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+
+}
